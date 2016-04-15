139c53e7fc1366f656a89d66ec0f91561483e387
Josh Miramant <josh@miramant.me>
2016-04-15 00:55:41 -0400

    content modules: updated node-v5

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ export const analytics = {
 
 export const auth = {
 
-  jwt: { secret: process.env.JWT_SECRET || 'React Starter Kit' },
+  jwt: { secret: process.env.JWT_SECRET || 'uiba-super-secret-key' },
 
   // https://developers.facebook.com/
   facebook: {
